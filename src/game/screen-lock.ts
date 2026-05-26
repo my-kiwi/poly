9573e08c1b9f54606cd20d84d295703614cb3fd7
my-kiwi <229952370+my-kiwi@users.noreply.github.com>
2026-05-26 12:18:17 +0000

    refactor: do not re-acquire wake lock if already acquired

--- src/game/screen-lock.ts
+++ src/game/screen-lock.ts
@@ -1,12 +1,14 @@
 let wakeLock: WakeLockSentinel | null = null;
 
 export async function requestWakeLock() {
+  if (wakeLock) return;
   if ('wakeLock' in navigator) {
     try {
       wakeLock = await navigator.wakeLock.request('screen');
       wakeLock.addEventListener('release', () => {
         console.log('Wake Lock released');
       });
+      console.log('Wake Lock acquired');
     } catch (err) {
       console.error('Wake Lock error:', err);
     }

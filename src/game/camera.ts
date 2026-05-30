785f648c3df8d31b9e2039be50c79f680d767021
my-kiwi <229952370+my-kiwi@users.noreply.github.com>
2026-05-30 11:20:45 +0000

    fix: adjust camera perspective and orbit controls settings

--- src/game/camera.ts
+++ src/game/camera.ts
@@ -3,12 +3,12 @@ import * as THREE from 'three';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 export const createCamera = () => {
-  const camera = new PerspectiveCamera(120, 1, 0.1, 200);
+  const camera = new PerspectiveCamera(80, 1, 0.1, 200);
   camera.position.set(0, 50, 0);
   camera.lookAt(new Vector3(0, 3, 0));
   // Orbit controls for free camera mode
   const orbitControls = new OrbitControls(camera, document.body);
-  orbitControls.maxDistance = 90;
+  orbitControls.maxDistance = 50;
   orbitControls.maxPolarAngle = THREE.MathUtils.degToRad(90);
   orbitControls.target.set(0, 0, 0);
   orbitControls.enabled = true;
